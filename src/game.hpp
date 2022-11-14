7f87b60e2f5faeb5e68ac835aabf2d9694bee22e
Douglas S. Elias <douglassantoselias@gmail.com>
2022-11-14 11:59:38 -0300

    format game.hpp file

--- src/game.hpp
+++ src/game.hpp
@@ -5,32 +5,33 @@
 #include "entity.hpp"
 #include "entity_manager.hpp"
 
-class Game {
+class Game
+{
   sf::RenderWindow window;
   EntityManager entities;
-sf::Font font;
-sf:Text text;
-int score =0;
-int currentFrame =0;
-int lastEnemySpawnTime= 0;
-bool paused =false;
-bool running = true;
+  sf::Font font;
+  sf::Text text;
+  int score = 0;
+  int currentFrame = 0;
+  int lastEnemySpawnTime = 0;
+  bool paused = false;
+  bool running = true;
 
-std::shared_ptr<Entity>player;
+  std::shared_ptr<Entity> player;
 
-void init(const std:: string& config);
-void setPaused(bool paused);
+  void init(const std::string &config);
+  void setPaused(bool paused);
 
-void movement();
-void userInput();
-void lifespan();
-void render();
-void enemySpawner();
-void collision();
+  void movement();
+  void userInput();
+  void lifespan();
+  void render();
+  void enemySpawner();
+  void collision();
 
 public:
-void spawnPlayer();
-void spawnEnemy();
+  void spawnPlayer();
+  void spawnEnemy();
 
-void run();
+  void run();
 };
